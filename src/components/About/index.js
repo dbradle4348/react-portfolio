25f0e36fd0590f1e3bf725a63bd829e1c05cf239
dbradle4348 <73002887+dbradle4348@users.noreply.github.com>
2021-04-04 23:27:32 -0600

    about finished, nav almost

--- src/components/About/index.js
+++ src/components/About/index.js
@@ -9,7 +9,7 @@ function About() {
       <img src={coverImage} className="my-2" style={{ width: "100%" }} alt="cover" />
       <div className="my-2">
         <p>
-          Lorem ipsum dolor sit amet, consectetur adipiscing elit. Donec bibendum turpis sed ex condimentum molestie. Mauris condimentum lectus ut ornare dignissim. Mauris faucibus urna mi, ac feugiat metus aliquam maximus. Proin aliquam justo nec diam vulputate vestibulum. Aenean sollicitudin nulla at nisi ornare, nec suscipit massa eleifend. Morbi tristique justo vel turpis sollicitudin, et tristique velit convallis. In hac habitasse platea dictumst. Phasellus mattis nunc sed orci consequat laoreet. Praesent id nisl nibh. Curabitur imperdiet ultricies mollis. In hac habitasse platea dictumst.
+          Hi! I'm a full stack web developer, national guard veteran, musical theater performer, and long-time hiking enthusiast. Check out some of my work in the links above and if you have any questions or would like to hire me for a project, hit that contact button and let's chat!
       </p>
       </div>
     </section>
