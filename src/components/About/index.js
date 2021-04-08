00e4a58c8405aef1abe82f6d7eb756652590bdbf
dbradle4348 <73002887+dbradle4348@users.noreply.github.com>
2021-04-08 11:23:21 -0600

    fixed some about me and portfolio things

--- src/components/About/index.js
+++ src/components/About/index.js
@@ -1,16 +1,18 @@
 import React from 'react'
 import coverImage from "../../assets/cover/cover-image.png";
+import DanielBradleyHeadshot from "../../assets/cover/DanielBradleyHeadshot.JPG"
 
 function About() {
 
   return (
     <section className="my-5" >
-      <h1 id="about">About Me</h1>
       <img src={coverImage} className="my-2" style={{ width: "100%" }} alt="cover" />
-      <div className="my-2">
+      <h1 id="about">About Me</h1>
+      <div className="my-2 flex-row">
+        <img src={DanielBradleyHeadshot} className="my-2" style={{ width: "25%" }} alt="headshot" />
         <p>
           Hi! I'm a full stack web developer, national guard veteran, musical theater performer, and long-time hiking enthusiast. Check out some of my work in the links above and if you have any questions or would like to hire me for a project, hit that contact button and let's chat!
-      </p>
+        </p>
       </div>
     </section>
   )
--- src/components/Portfolio/index.js
+++ src/components/Portfolio/index.js
@@ -9,31 +9,35 @@ function Portfolio() {
     return (
     <section>
       <h1 id="portfolio">Portfolio</h1>
-      <p>Some of my work:</p>
+      <p>Here's some of my work! Click on the image to see the deployed application or the image title to see the GitHub repository. If the application is for Node.js only, the link will take you to the repository where you can find installation information in the corresponding ReadMe.</p>
     <div className='flex-row px-2 space-between'>
         <div>
         <a href="https://kenneththelarson.github.io/urban-fiesta/" target="_blank" rel="noopener noreferrer">
             <img src={Whiskey_Ice} alt="urban-fiesta" width='300'/>
         </a>
-            <p>Urban Fiesta</p>
+            <a href="https://github.com/kenneththelarson/urban-fiesta" target="_blank" rel="noopener noreferrer">
+            <p>Urban Fiesta</p>    
+        </a>
         </div>
         <div>
         <a href="https://gym-rats-1.herokuapp.com/" target="_blank" rel="noopener noreferrer">
             <img src={weights} alt="gym-rats" width='300'/>
         </a>
+        <a href="https://github.com/bbec0689/gym-rats" target="_blank" rel="noopener noreferrer">
             <p>Gym Rats</p>
+            </a>
         </div>
         <div>
         <a href="https://github.com/dbradle4348/my-team" target="_blank" rel="noopener noreferrer">
             <img src={myTeam} alt="my-team" width='300'/>
-        </a>
             <p>My Team</p>
+        </a>    
         </div>
         <div>
         <a href="https://github.com/dbradle4348/README-Generator" target="_blank" rel="noopener noreferrer">
             <img src={githubImage} alt="readme-generator" width='300'/>
-        </a>
             <p>ReadMe Generator</p>
+        </a>    
         </div>
         <div>
         <a href="https://gym-rats-1.herokuapp.com/" target="_blank" rel="noopener noreferrer">
