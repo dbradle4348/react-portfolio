d78dbd8cf568a439511ff19497818293737419b4
dbradle4348 <73002887+dbradle4348@users.noreply.github.com>
2021-04-08 18:28:45 -0600

    fixed resume

--- src/components/Resume/index.js
+++ src/components/Resume/index.js
@@ -1,11 +1,13 @@
 import React from 'react';
-import danielbradley1pageresume from '../../assets/Resume/danielbradley1pageresume.pdf';
+import danielbradley1pageresume from '../../assets/Resume/danielbradley1pageresume.docx.pdf'
 
 function Resume() {
     return (
         <section>
         <h1 id="resume">Resume</h1>
-        <p>Download one page resume here: <a href={danielbradley1pageresume} target="_blank" download>Download Resume</a></p>
+        <p>Download one page resume here: <a href={danielbradley1pageresume} target="_blank" rel="noreferrer" download>Download Resume</a></p>
+        <p>Developer Proficiencies:</p>
+        <p>HTML, CSS, Javascript, Node.js, React, Mongodb, SQL, [I'll fill this out better later]</p>
         </section>
     )
 }
